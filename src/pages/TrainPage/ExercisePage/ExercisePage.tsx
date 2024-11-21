2d20c2de44deac260eb11efd0372485dfad07250
Baltukov Nomto <nomto_04@mail.ru>
2024-11-21 19:50:59 +0800

    Сделал exercises

--- src/pages/TrainPage/ExercisePage/ExercisePage.tsx
+++ src/pages/TrainPage/ExercisePage/ExercisePage.tsx
@@ -1,7 +1,45 @@
+import workoutImg1 from "../../../assets/img/workouts/1.png";
 export default function ExercisePage() {
   return (
-    <div>
-      <h1>Страница с упражнениями</h1>
+    <div className="exercise-page">
+      <div className="workouts">
+        <div className="workout">
+          <h3 className="workout__title">Тренировка #1</h3>
+          <div className="workout__content">
+            <div className="workout__picture">
+              <img src={workoutImg1} alt="" />
+            </div>
+            <div className="workout__text">
+              Найдете баланс в тренировках — найдете его в жизни. В этом поможет
+              ком..
+            </div>
+          </div>
+        </div>
+        <div className="workout">
+          <h3 className="workout__title">Тренировка #2</h3>
+          <div className="workout__content">
+            <div className="workout__picture">
+              <img src={workoutImg1} alt="" />
+            </div>
+            <div className="workout__text">
+              Найдете баланс в тренировках — найдете его в жизни. В этом поможет
+              ком..
+            </div>
+          </div>
+        </div>
+        <div className="workout">
+          <h3 className="workout__title">Тренировка #3</h3>
+          <div className="workout__content">
+            <div className="workout__picture">
+              <img src={workoutImg1} alt="" />
+            </div>
+            <div className="workout__text">
+              Найдете баланс в тренировках — найдете его в жизни. В этом поможет
+              ком..
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
