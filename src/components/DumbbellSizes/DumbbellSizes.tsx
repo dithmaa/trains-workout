e158a394447ffc01c224826898ed376ff044355f
Baltukov Nomto <nomto_04@mail.ru>
2024-11-24 18:19:57 +0800

    Страницы гантелей

--- src/components/DumbbellSizes/DumbbellSizes.tsx
+++ src/components/DumbbellSizes/DumbbellSizes.tsx
@@ -1,22 +1,45 @@
+import React from "react";
 import styles from "./DumbbellSizes.module.scss";
 export const DumbbellSizes = () => {
+  const weights = [
+    "1 кг",
+    "2 кг",
+    "2.5 кг",
+    "4 кг",
+    "5 кг",
+    "6 кг",
+    "7 кг",
+    "7.5 кг",
+    "8 кг",
+    "8.5 кг",
+    "9 кг",
+    "10 кг",
+    "11 кг",
+    "12 кг",
+    "12.5 кг",
+  ];
+  const [activeIndexes, setActiveIndexes] = React.useState<number[]>([]); // Индекс активного элемента
+  const handleClick = (index: number) => {
+    setActiveIndexes(
+      (prev) =>
+        prev.includes(index)
+          ? prev.filter((i) => i !== index) // Убираем индекс
+          : [...prev, index] // Добавляем индекс
+    );
+  };
   return (
     <div className={styles.root}>
-      <div className={styles.root__item}>1 кг</div>
-      <div className={styles.root__item}>2 кг</div>
-      <div className={styles.root__item}>2.5 кг</div>
-      <div className={styles.root__item}>4 кг</div>
-      <div className={styles.root__item}>5 кг</div>
-      <div className={styles.root__item}>6 кг</div>
-      <div className={styles.root__item}>7 кг</div>
-      <div className={styles.root__item}>7.5 кг</div>
-      <div className={styles.root__item}>8 кг</div>
-      <div className={styles.root__item}>8.5 кг</div>
-      <div className={styles.root__item}>9 кг</div>
-      <div className={styles.root__item}>10 кг</div>
-      <div className={styles.root__item}>11 кг</div>
-      <div className={styles.root__item}>12 кг</div>
-      <div className={styles.root__item}>12.5 кг</div>
+      {weights.map((weight, index) => (
+        <div
+          key={index}
+          className={`${styles.root__item} ${
+            activeIndexes.includes(index) ? "active" : ""
+          }`}
+          onClick={() => handleClick(index)} // Обработчик клика
+        >
+          {weight}
+        </div>
+      ))}
     </div>
   );
 };
